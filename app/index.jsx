ad9b68fb8ffa8fcb5f4d2ff04173feac41a99195
Aditi Upadhyay <au.aditi08@gmail.com>
2025-08-02 13:12:40 +0530

    modify file structure to implement tailwindcss

--- app/index.jsx
+++ app/index.jsx
@@ -3,13 +3,14 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
+      className="bg-red-600"
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-purple-400 text-2xl">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
