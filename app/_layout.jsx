154d8e0acbf025be58ce25e965035cdf0b545a27
Aditi Upadhyay <au.aditi08@gmail.com>
2025-08-05 15:50:59 +0530

    define stacks

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,10 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { Text } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{headerShown:false}} >
+    <Stack.Screen name="index" /> 
+    <Stack.Screen  name="(tabs)"/>
+  </Stack>;
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+
+  const router = useRouter()
+
   return (
     <View
-      className="bg-red-600"
+      // className="bg-red-600"
       style={{
         flex: 1,
         justifyContent: "center",
@@ -11,6 +15,11 @@ export default function Index() {
       }}
     >
       <Text className="text-purple-400 text-2xl">Edit app/index.tsx to edit this screen.</Text>
+      <TouchableOpacity onPress={()=> router.push("/testing")} >
+        <Text>
+          change route
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
