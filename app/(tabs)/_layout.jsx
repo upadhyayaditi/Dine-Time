94b29bf5f84d43f6228547b926ae40b5fdb7065a
Aditi Upadhyay <au.aditi08@gmail.com>
2025-08-06 11:57:36 +0530

    style the tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,13 +1,31 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { Colors } from "../../assets/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const TabLayout = () => {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="home" options={{ title: "Home" }} />
-      <Tabs.Screen name="history" options={{ title: "History" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: Colors.PRIMARY, tabBarInactiveTintColor: Colors.dark.text,
+        tabBarStyle:{
+            backgroundColor: Colors.SECONDARY,
+            paddingBottom: 14,
+            height: 75
+        },
+        tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "bold"
+        }
+     }}>
+      <Tabs.Screen name="home" options={{ title: "Home", tabBarIcon: ({color})=>(
+        <Ionicons name="home" size={24} color={color} />
+      ) }} />
+      <Tabs.Screen name="history" options={{ title: "History",  tabBarIcon: ({color})=>(
+        <Ionicons name="timer" size={24} color={color} />
+      ) }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile", tabBarIcon: ({color})=>(
+        <Ionicons name="person-sharp" size={24} color={color} />
+      ) }} />
     </Tabs>
   );
 };
