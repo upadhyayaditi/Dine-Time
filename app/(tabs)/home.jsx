036a609b23ff624936c3c1d01fd4291071f22de7
Aditi Upadhyay <au.aditi08@gmail.com>
2025-10-16 16:03:51 +0530

    implement expo-blur in home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,7 +1,10 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Platform, ScrollView } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import logo from "../../assets/images/dinetimelogo.png";
+import banner from "../../assets/images/homeBanner.png";
+import { ImageBackground } from "react-native-web";
+import { BlurView } from "expo-blur";
 
 const home = () => {
   return (
@@ -9,11 +12,35 @@ const home = () => {
       <View className="flex items-center  ">
         <View className=" bg-[#5f5f5f] w-11/12 rounded-lg shadow-lg justify-between items-center flex flex-row p-2 ">
           <View className="flex flex-row">
-            <Text>Welcome to{" "}</Text>
+            <Text
+              className={`text-base h-10 pt-[${
+                Platform.OS == "android" ? 8 : 10
+              }] align-middle text-white`}
+            >
+              {" "}
+              Welcome to{" "}
+            </Text>
             <Image resizeMode="cover" className={"w-20 h-12"} source={logo} />
           </View>
         </View>
       </View>
+      <ScrollView>
+        <ImageBackground
+          resizeMode="cover"
+          className="my-4 w-full h-52 items-center justify-center"
+          source={banner}
+        >
+          <BlurView
+            intensity={Platform.OS === "android" ? 100 : 25}
+            tint="dark"
+            className="w-full p-4 shadow-lg "
+          >
+            <Text className=" text-center text-3xl font-bold text-white ">
+              Dine with your loved ones
+            </Text>
+          </BlurView>
+        </ImageBackground>
+      </ScrollView>
     </SafeAreaView>
   );
 };
